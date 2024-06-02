a506c7c6a7f849db9ce7cef79405be04b2703127
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-06-02 16:01:02 +0500

    refactor: add bound for IdT

--- todoapp/domain/common/entities.py
+++ todoapp/domain/common/entities.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-from typing import TypeVar
 from typing import TypeVar, Generic
+from uuid import UUID
 
 from pydantic import BaseModel
 
 from todoapp.domain.common.value_objects import DateTimeNull
 
-IdT = TypeVar('IdT')
+IdT = TypeVar('IdT', bound=UUID)
 
 
 class BaseEntity(BaseModel, Generic[IdT]):
