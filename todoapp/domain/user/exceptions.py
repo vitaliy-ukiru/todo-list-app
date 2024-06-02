7146cf147e94d34c6dbc14e94f38f72be64a03b5
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-06-02 13:21:02 +0500

    feat: add user domain errors

---
+++ todoapp/domain/user/exceptions.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+from todoapp.domain.common.exceptions import DomainError
+from todoapp.domain.user.entities import UserId
+
+
+@dataclass(eq=False)
+class UserIsDeletedError(RuntimeError, DomainError):
+    user_id: UserId
+
+    @property
+    def title(self) -> str:
+        return f'The user with "{self.user_id}" user_id is deleted'
+
+
+@dataclass(eq=False)
+class EmailAlreadyExistsError(DomainError):
+    email: str | None = None
+
+    @property
+    def title(self) -> str:
+        if self.email is None:
+            return "A user with the email already exists"
+        return f'A user with the "{self.email}" email already exists'
