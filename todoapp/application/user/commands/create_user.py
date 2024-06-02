945c76a8005c9ba3e0a431a9ce74dfa003f71dd6
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-06-02 15:57:36 +0500

    feat(user): add create user command

---
+++ todoapp/application/user/commands/create_user.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+from didiator import Mediator
+
+from todoapp.application.common.command import Command, CommandHandler
+from todoapp.application.common.interfaces.uow import UnitOfWork
+from todoapp.application.user.interfaces.repo import UserRepo
+from todoapp.domain.user.entities import PasswordHasher, User
+
+
+@dataclass
+class CreateUser(Command[UUID]):
+    email: str
+    password: str
+
+
+class CreateUserHandler(CommandHandler[CreateUser, UUID]):
+    def __init__(
+        self,
+        user_repo: UserRepo,
+        uow: UnitOfWork,
+        mediator: Mediator,
+        hasher: PasswordHasher,
+    ) -> None:
+        self._hasher = hasher
+        self._user_repo = user_repo
+        self._uow = uow
+        self._mediator = mediator
+
+    async def __call__(self, command: CreateUser) -> UUID:
+        user = User.create(command.email, command.password, self._hasher)
+        await self._user_repo.save_user(user)
+        await self._uow.commit()
+
+        return user.id
--- todoapp/domain/user/entities.py
+++ todoapp/domain/user/entities.py
@@ -1,13 +1,52 @@
-from typing import NewType, Annotated
+import abc
+from datetime import datetime
+from typing import NewType, Annotated, Protocol, Self
 from uuid import UUID
 
 from pydantic import EmailStr, SecretStr, Field
+from uuid6 import uuid7
 
 from todoapp.domain.common.entities import BaseEntity
 
 UserId = NewType("UserId", UUID)
 
 
+class PasswordHasher(Protocol):
+    @abc.abstractmethod
+    def hash(self, password: str) -> str:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def verify(self, hashed_password: str, password: str) -> bool:
+        raise NotImplementedError
+
+
 class User(BaseEntity[UserId]):
     email: EmailStr
-    password: Annotated[SecretStr, Field(min_length=8, exclude=True)]
+    password: Annotated[SecretStr, Field(exclude=True)]
+
+    @classmethod
+    def create(
+        cls,
+        email: str,
+        password: str,
+        hasher: PasswordHasher
+    ) -> Self:
+        creation_time = datetime.utcnow()
+        user_id = UserId(uuid7())
+        return cls(
+            id=user_id,
+            email=email,
+            password=hasher.hash(password),
+            created_at=creation_time,
+        )
+
+    def verify_password(self, hasher: PasswordHasher, password: str) -> bool:
+        return hasher.verify(self.password.get_secret_value(), password)
+
+    @property
+    def is_deleted(self) -> bool:
+        return self.deleted_at is not None
+
+    def delete(self):
+        self.deleted_at = datetime.utcnow()
