6f64c4390d1d2d1b1275953ab69f1ad0c4eec44c
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-06-03 03:41:50 +0500

    style: delete unused import

--- todoapp/domain/common/entities.py
+++ todoapp/domain/common/entities.py
@@ -4,12 +4,9 @@ from uuid import UUID
 
 from pydantic import BaseModel
 
-from todoapp.domain.common.value_objects import DateTimeNull
-
 IdT = TypeVar('IdT', bound=UUID)
 
 
 class BaseEntity(BaseModel, Generic[IdT]):
     id: IdT
     created_at: datetime
-
