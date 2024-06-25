171e10cd848ef0732eb38846f122717104365fb7
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-06-25 17:12:57 +0500

    feat: add auth config

--- todoapp/common/settings.py
+++ todoapp/common/settings.py
@@ -1,12 +1,14 @@
 __all__ = (
     "Config",
     "DatabaseConfig",
+    "AuthConfig",
 )
+
+from datetime import timedelta
 from typing import Annotated
 
+from pydantic import SecretStr, Field, RedisDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import SecretStr, Field
-
 
 ENV_FILE = ".env"
 EXTRA = "ignore"
@@ -21,6 +23,7 @@ def _model_config(prefix: str = "") -> SettingsConfigDict:
         case_sensitive=False,
     )
 
+
 class DatabaseConfig(BaseSettings):
     password: SecretStr | None = None
     username: str
@@ -31,8 +34,20 @@ class DatabaseConfig(BaseSettings):
     model_config = _model_config("db_")
 
 
+class AuthConfig(BaseSettings):
+    access_token_expires: Annotated[timedelta, Field(default=timedelta(minutes=5))]
+    refresh_token_expires: Annotated[timedelta, Field(default=timedelta(days=30))]
+
+    private_key_path: str
+    jwt_alg: Annotated[str, Field(default="RS256")]
+
+    model_config = _model_config("auth_")
+
+
 class Config(BaseSettings):
     db: Annotated[DatabaseConfig, Field(default_factory=DatabaseConfig)]
+    auth: Annotated[AuthConfig, Field(default_factory=AuthConfig)]
+    redis_url: Annotated[RedisDsn, Field(default="redis://127.0.0.1:6379")]
     host: str = "localhost"
     port: int = 8000
 
