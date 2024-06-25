bece0945028d954dfb0f060f06059172155fc942
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-06-25 17:22:55 +0500

    chore: update user repo implementation

--- todoapp/infrastructure/db/repositories/user.py
+++ todoapp/infrastructure/db/repositories/user.py
@@ -1,12 +1,17 @@
 from typing import NoReturn
 
+from pydantic import SecretStr
+from sqlalchemy import select
 from sqlalchemy.exc import DBAPIError, IntegrityError
 
 from todoapp.application.common.exceptions import RepoError
-from todoapp.application.user.exceptions import UserIdAlreadyExistsError, UserIdNotExistError
+from todoapp.application.user import dto
+from todoapp.application.user.exceptions import UserIdAlreadyExistsError, UserIdNotExistError, \
+    EmailNotExistError
 from todoapp.application.user.interfaces import UserRepo
 from todoapp.domain.user import entities
-from todoapp.domain.user.exceptions import EmailAlreadyExistsError
+from todoapp.domain.user.entities import UserId
+from todoapp.domain.user.exceptions import EmailAlreadyExistsError, UserIsDeletedError
 from todoapp.infrastructure.db.models import User
 from .base import SQLAlchemyRepo
 from .exception_mapper import exception_mapper
@@ -14,7 +19,7 @@ from .exception_mapper import exception_mapper
 
 class UserRepoImpl(SQLAlchemyRepo, UserRepo):
     @exception_mapper
-    async def acquire_user_by_id(self, user_id: entities.UserId) -> entities.User:
+    async def get_user_by_id(self, user_id: entities.UserId) -> dto.User:
         user: User | None = await self._session.get(
             User,
             user_id,
@@ -22,6 +27,16 @@ class UserRepoImpl(SQLAlchemyRepo, UserRepo):
         )
         if user is None:
             raise UserIdNotExistError(user_id)
+
+        return convert_model_to_entity(user)
+
+    @exception_mapper
+    async def acquire_user_by_email(self, email: str) -> entities.User:
+        user: User | None = await self._session.scalar(
+            select(User).where(User.email == email)
+        )
+        if user is None:
+            raise EmailNotExistError(email)
 
         return convert_model_to_entity(user)
 
@@ -55,11 +70,21 @@ class UserRepoImpl(SQLAlchemyRepo, UserRepo):
 
 def convert_model_to_entity(user: User) -> entities.User:
     return entities.User(
-        id=user.id,
+        id=UserId(user.id),
         email=user.email,
-        password=user.password_hash,
+        password=SecretStr(user.password_hash),
         created_at=user.created_at,
         deleted_at=user.deleted_at,
+    )
+
+def convert_model_to_dto(user: User) -> dto.User:
+    user_id = UserId(user.id)
+    if user.deleted_at is not None:
+        raise UserIsDeletedError(user_id)
+
+    return dto.User(
+        id=user_id,
+        email=user.email
     )
 
 
