aba020097ecbac4d4b612f4ba937ba19ee11c2bd
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-06-28 02:14:53 +0500

    chore: add response class

---
+++ todoapp/presentation/api/controllers/responses/base.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from typing import Generic, TypeVar, Annotated
+
+from pydantic import BaseModel, Field
+
+from todoapp.domain.common.exceptions import AppError
+
+TResult = TypeVar("TResult")
+TError = TypeVar("TError")
+
+
+class Response(BaseModel):
+    ok: bool
+
+
+class OkResponse(Response, Generic[TResult]):
+    ok: Annotated[bool, Field(default=True, init=False)]
+    result: TResult | None = None
+
+
+class ErrorData(BaseModel, Generic[TError]):
+    code: str = "UNKNOWN_ERROR"
+    title: str = "Unknown error occurred"
+    status: int = 500
+    data: TError | None = None
+
+    @classmethod
+    def from_error(cls, err: AppError, status_code: int):
+        return cls(
+            code=err.code,
+            title=err.title,
+            data=err,
+            status=status_code,
+        )
+
+
+class ErrorResponse(Response, Generic[TError]):
+    ok: Annotated[bool, Field(default=False, init=False)]
+    error: Annotated[ErrorData[TError], Field(default_factory=ErrorData)]
+
+
+@dataclass(frozen=True)
+class OkStatus:
+    status: str = "ok"
+
+
+OK_STATUS = OkStatus()
