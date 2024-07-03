f619943a00a11dfb72218d16fec0ad84f16563ca
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-07-03 16:48:47 +0500

    chore: add redirect to docs

---
+++ todoapp/presentation/api/controllers/base.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from starlette import status
+from starlette.responses import RedirectResponse
+
+base_router = APIRouter()
+
+@base_router.get("/", include_in_schema=False)
+def redirect_to_docs():
+    return RedirectResponse(url="/docs", status_code=status.HTTP_308_PERMANENT_REDIRECT)
--- todoapp/presentation/api/controllers/main.py
+++ todoapp/presentation/api/controllers/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from .auth import auth_router
+from .base import base_router
 from .healthcheck import healthcheck_router
 from .task import task_router
 from .task_list import task_list_router
@@ -8,6 +9,7 @@ from .user import user_router
 
 
 def setup_controllers(app: FastAPI):
+    app.include_router(base_router)
     app.include_router(healthcheck_router)
     app.include_router(user_router)
     app.include_router(auth_router)
