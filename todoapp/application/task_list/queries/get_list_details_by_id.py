0b9c53ccda2ae64103068f51e39656600a812b5d
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-07-03 13:53:12 +0500

    feat: add check to access to list

--- todoapp/application/task_list/queries/get_list_details_by_id.py
+++ todoapp/application/task_list/queries/get_list_details_by_id.py
@@ -2,14 +2,17 @@ from dataclasses import dataclass
 from uuid import UUID
 
 from todoapp.application.common.query import Query, QueryHandler
+from todoapp.application.task_list.exceptions import TaskListAccessError
 from todoapp.application.task_list.interfaces import TaskListRepo
 from todoapp.domain.tasks_list.entities import TaskListDetails
 from todoapp.domain.tasks_list.value_objects import ListId
+from todoapp.domain.user.entities import UserId
 
 
 @dataclass(frozen=True)
 class GetListDetailsById(Query[TaskListDetails]):
     list_id: UUID
+    user_id: UUID
 
 
 @dataclass
@@ -19,4 +22,7 @@ class GetListDetailsByIdHandler(QueryHandler[GetListDetailsById, TaskListDetails
     async def __call__(self, query: GetListDetailsById) -> TaskListDetails:
         list_id = ListId(query.list_id)
         task_list = await self.list_repo.acquire_task_list_by_id(list_id)
+        if not task_list.is_have_access(UserId(query.user_id)):
+            raise TaskListAccessError(query.list_id)
+
         return task_list
