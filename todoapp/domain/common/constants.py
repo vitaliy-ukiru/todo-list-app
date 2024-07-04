91b3addba34c3ec8f0a1a4c888448834238b3ae7
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-07-04 23:54:12 +0500

    feat: add operations enum

--- todoapp/domain/common/constants.py
+++ todoapp/domain/common/constants.py
@@ -1,5 +1,16 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class Empty(Enum):
     UNSET = "UNSET"
+
+
+class Operation(Enum):
+    read = auto()
+    update_task_list = auto()
+    delete_task_list = auto()
+
+    update_task = auto()
+
+    add_task_to_list = auto()
+    delete_task_from_list = auto()
