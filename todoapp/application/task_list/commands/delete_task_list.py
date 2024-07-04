d60aeea3b7a0c7fd88e12ac9f4d66d2fe819937e
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-07-05 00:00:58 +0500

    fix: raise wrong error

--- todoapp/application/task_list/commands/delete_task_list.py
+++ todoapp/application/task_list/commands/delete_task_list.py
@@ -3,8 +3,9 @@ from uuid import UUID
 
 from todoapp.application.common.command import Command, CommandHandler
 from todoapp.application.common.interfaces.uow import UnitOfWork
-from todoapp.application.task.exceptions import TaskAccessError
+from todoapp.application.task_list.exceptions import TaskListAccessError
 from todoapp.application.task_list.interfaces import TaskListRepo
+from todoapp.domain.common.constants import Operation
 from todoapp.domain.tasks_list.value_objects import ListId
 from todoapp.domain.user.entities import UserId
 
@@ -25,8 +26,8 @@ class DeleteTaskListHandler(CommandHandler[DeleteTaskList, None]):
         list_id = ListId(command.list_id)
 
         task_list = await self.list_repo.acquire_task_list_by_id(list_id)
-        if not task_list.is_have_access(user_id):
-            raise TaskAccessError(list_id)
+        if not task_list.is_have_access(user_id, Operation.delete_task_list):
+            raise TaskListAccessError(list_id)
 
         await self.list_repo.delete_task_list(list_id)
         await self.uow.commit()
