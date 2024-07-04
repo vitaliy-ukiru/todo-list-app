b4ef53c28c8cd00b8693f70ddb02bb25271635be
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-07-05 00:14:04 +0500

    chore: FindTaskLists in __all__

--- todoapp/application/task_list/queries/__init__.py
+++ todoapp/application/task_list/queries/__init__.py
@@ -1,7 +1,8 @@
 from .get_list_by_id import GetListById, GetListByIdHandler
 from .get_list_details_by_id import GetListDetailsById, GetListDetailsByIdHandler
-
+from .find_tasks_lists import FindTaskLists, FindTaskListsHandler
 __all__ = (
     GetListDetailsById, GetListDetailsByIdHandler,
     GetListById, GetListByIdHandler,
+    FindTaskLists, FindTaskListsHandler,
 )
