bbd6e8b0da4eb11c721ed04c829a8d6f06fbce3f
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-08-12 22:34:32 +0500

    fix: docker compose

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,2 +1,15 @@
-alembic upgrade head
-poetry run python -m todoapp
+#!/usr/bin/env bash
+
+set -e
+
+APP="exec poetry run python -m todoapp"
+
+
+function migrate () {
+  if [[ ! -z "${RUN_MIGRATIONS}" ]]; then
+    alembic upgrade head
+  fi
+}
+
+migrate
+${APP} $*
